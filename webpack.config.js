ccd84d2089190972bafe303efd7a4270e93d079b
Halil Bilgil <hbilgil@gmail.com>
2022-07-31 12:30:21 +0300

    Edit config file for devtools

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  watch: true,
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
